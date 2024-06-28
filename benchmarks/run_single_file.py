d99eecc14030715b6d4841971e5e18d797f8b928
許湛然(Jeff Hsu) <a24998667@gmail.com>
2024-06-29 00:33:18 +0800

    Create run_single_file.py

---
+++ benchmarks/run_single_file.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+from gfd.gfd import Breezper
+from gfd.utils import process_config
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Override config settings with command-line arguments.")
+    parser.add_argument('--config_file_path', type=str, default='config_files/default_config.yaml', help='The default config for running single file')
+    parser.add_argument('--audio_file_path', type=str, help='Path to the audio file sample')
+    parser.add_argument('--result_output_path', type=str, help='Path to save dataset with predictions from the model')
+    return parser.parse_args()
