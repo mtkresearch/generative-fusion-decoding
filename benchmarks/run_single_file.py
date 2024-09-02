8fa5d3bd082fed28e98c8a509b793df641f94904
許湛然(Jeff Hsu) <a24998667@gmail.com>
2024-09-02 18:07:48 +0800

    fix function typo

--- benchmarks/run_single_file.py
+++ benchmarks/run_single_file.py
@@ -30,7 +30,7 @@ def main():
     combined_config = combine_config(prompt_config, setting_config)
     
     model = Breezper(combined_config)
-    result = model.get_transcription(args.audio_file_path, combined_config.asr_prompt, combined_config.llm_prompt)
+    result = model.get_transcription(args.audio_file_path, asr_prompt=combined_config.asr_prompt, llm_prompt=combined_config.llm_prompt)
     print(f'Result: {result}')
 
     with open(args.result_output_path, 'w') as f:
